0ea7aa75d8119c3a77eaf7cc268590c2ee87ccfc
mgrizqy <mgrizqy.work@gmail.com>
2025-07-23 13:58:20 +0700

    page.tsx - added divider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,18 @@ export default function Home() {
   return (
 <div>
   <h1>This is landing page</h1>
+
+    {/* Arco - start */}
+
+    {/* Arco - end */}
+
+
+
+    {/* Eky - start */}
+
+    {/* Eky - end */}
+
+
 </div>
   );
 }
